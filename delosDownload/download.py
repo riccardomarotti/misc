de088bcc05a74a75c97c7015a188ac390ee9e287
Riccardo Marotti <riccardo.marotti@gmail.com>
2019-07-21 16:58:48 +0200

    Save delos file with original filename

--- delosDownload/download.py
+++ delosDownload/download.py
@@ -1,6 +1,7 @@
 import sys
 import getopt
-from requests import get
+import re
+from requests import get, head
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
@@ -9,16 +10,16 @@ from bs4 import BeautifulSoup
 def main(cookie):
     cookies = {'DelosID': cookie}
 
-    for n in range(1, 208):
+    for n in range(1, 209):
         html = get(
             f'https://www.fantascienza.com/delos/{n}', cookies=cookies).content
         links = BeautifulSoup(html, 'html.parser').findAll(
             "div", {"class": "download-version"})
         for l in links:
             delosEpubLink = l.select("a")[0]['href']
+            filename = re.findall("filename=\"(.+)\"", head(delosEpubLink).headers['content-disposition'])[0]
             delosEbub = get(delosEpubLink)
 
-            filename = f"./delos-{n:03d}.zip"
             print(f"creating {filename}...")
             with open(filename, 'wb') as f:
                 f.write(delosEbub.content)
